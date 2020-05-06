7357a1565dbe41569bd59635d08e2aab0c4b2c57
Benoit Adam <benoit.adam@yacare.fr>
2020-05-06 20:26:01 +1000

    Putting strings outside of files

---
+++ app/containers/Game/messages.js
@@ -0,0 +1,19 @@
+export const messages = {
+    header: 'Game screen',
+    settings: {
+        open: 'Open settings...'
+    },
+    startButton: {
+        start: 'Start game',
+        pause: 'Pause game',
+        resume: 'Resume game'
+    },
+    teamA: 'Home team',
+    teamB: 'Visitor team',
+    addGoal: 'Add a goal',
+    addYellowCard: 'Add a yellow card',
+    addSuspension: 'Add a 2 minutes',
+    addRedCard: 'Add a red card',
+    addBlueCard: 'Add a blue card',
+    addTimeout: 'Add a timeout'
+};
---
+++ app/containers/Settings/messages.js
@@ -0,0 +1,9 @@
+export const messages = {
+    header: 'Settings',
+    close: 'Close settings',
+    save: 'Save settings',
+    teamA: 'Home team',
+    teamB: 'Visitor team',
+    addPlayer: 'Add a player',
+    playerNumberAndName: 'Player number and name'
+};
