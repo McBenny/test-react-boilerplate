de4712e3a2163d74be4412a1550bfa5a0e0c12cc
Benoit Adam <benoit.adam@yacare.fr>
2020-05-21 22:53:53 +1000

    Renaming Splashscreen and starting tests

---
+++ app/components/Line-up/tests/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import LineUp from '../index';
+import { messages } from '../messages';
+
+const componentProperties = {
+    team: 'A',
+    playersList: [],
+    captainId: 0,
+    officialsList: [],
+    closeHandler: () => {},
+    openPopup: () => {}
+};
+
+describe('<LineUp />', () => {
+    it('should render and match the snapshot', () => {
+        const {
+            container: { firstChild }
+        } = render(<LineUp {...componentProperties} />);
+        expect(firstChild).toMatchSnapshot();
+    });
+});
+
+describe('LineUp messages', () => {
+    it('should be defined and have at least a title', () => {
+        expect(messages).toBeDefined();
+        expect(messages.title).toBeDefined();
+        expect(Object.keys(messages).length).toBeGreaterThan(1);
+    });
+});
---
+++ app/components/modal/tests/index.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Modal, { cancelButton } from '../index';
+import { messages } from '../messages';
+
+const componentProperties = {
+    team: 'A',
+    playersList: [],
+    captainId: 0,
+    officialsList: [],
+    closeHandler: () => {},
+    openPopup: () => {}
+};
+
+describe('Modal messages', () => {
+    it('should be defined and have at least Cancel and Close labels', () => {
+        expect(messages).toBeDefined();
+        expect(messages.cancel).toBeDefined();
+        expect(messages.cancel).not.toBe('');
+        expect(messages.close).toBeDefined();
+        expect(messages.close).not.toBe('');
+    });
+});
+
+describe('cancelButton func', () => {
+    it('should render and match the snapshot', () => {
+        const {
+            container: { firstChild }
+        } = render(cancelButton(componentProperties.closeHandler()));
+        expect(firstChild).toMatchSnapshot();
+    });
+
+    it('function is called when button close is clicked in modal', () => {
+        const onClickMock = jest.fn();
+        const { getByRole } = render(<Modal {...componentProperties} closeHandler={onClickMock} />);
+        fireEvent.click(getByRole('button'));
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+    });
+
+    // it('CLose function is called when keyPress', () => {
+    //     const onClickMock = jest.fn();
+    //     const { container } = render(<Modal {...componentProperties} closeHandler={onClickMock} />);
+    //     fireEvent.keyPress(container, { key: ESCAPE_KEY_NAME, code: ESCAPE_KEY_CODE });
+    //     expect(onClickMock).toHaveBeenCalledTimes(1);
+    // });
+
+    it('function is called when cancel button is clicked', () => {
+        const onClickMock = jest.fn();
+        const { getByRole } = render(cancelButton(onClickMock));
+        fireEvent.click(getByRole('button'));
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('<Modal />', () => {
+    it('should render and match the snapshot', () => {
+        const {
+            container: { firstChild }
+        } = render(<Modal {...componentProperties} />);
+        expect(firstChild).toMatchSnapshot();
+    });
+});
