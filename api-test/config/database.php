23c4cc705326f9d67fbc71a26b1a164fc960f23b
Benoit Adam <benoit.adam@yacare.fr>
2020-09-01 16:06:01 +1000

    Fixing security breach

--- api-test/config/database.php
+++ api-test/config/database.php
@@ -1,10 +1,23 @@
 <?php
+include_once './credentials.php';
+
 class Database {
-    private $host = "db5000510146.hosting-data.io";
-    private $db_name = "dbs489610";
-    private $username = "dbu631090";
-    private $password = "F2(mW<!?(X";
+//    private $host = "db5000510146.hosting-data.io";
+//    private $db_name = "dbs489610";
+//    private $username = "dbu631090";
+//    private $password = "F2(mW<!?(X";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     public $conn;
+
+    public function __construct() {
+        $this->host = $h1;
+        $this->db_name = $h2;
+        $this->user_name = $h3;
+        $this->password = $h4;
+    }
 
     public function getConnection() {
         $this->conn = null;
